a1948944ae42e3d8811d257ec80a7ab9c53ce79d
smit <smitkotkar42@gmail.com>
2024-01-22 18:27:58 +0530

    sg

--- instance.tf
+++ instance.tf
@@ -3,12 +3,38 @@ provider "aws" {
 }
 
 resource "aws_instance" "my-instance" {
-    count = 5
+    count = 3
     ami = "ami-0c2f3d2ee24929520"
     key_name = "ohio"
     instance_type = "t2.micro"
-    vpc_security_group_ids = ["sg-0bf1e4b2a2e7674af"]
+    vpc_security_group_ids = [ aws_security_group.my-sg.id ]
+        depends_on = [ aws_security_group.my-sg ]
     tags = {
-        Name = "Instance_1-${count.index + 1}"
+        Name = "Instance-${count.index + 1}"
+    }
+}
+
+resource "aws_security_group" "my-sg" {
+    name = "all-allow-sg"
+    description = "ALlow TLS inbound traffic"
+    vpc_id = "vpc-0bf146a55b2b621eb"
+
+    ingress {
+        description = "TLS from VPC"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "tcp"
+        cidr_block = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "allow_tls"
     }
 }
