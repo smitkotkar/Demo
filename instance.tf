b82292bc24d66bb30cea362966fad6486a0d7fd6
smit <smitkotkar42@gmail.com>
2024-01-22 17:31:56 +0530

    instance

---
+++ instance.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region = "us-east-2"
+}
+
+resource "aws_instance" "my-instance" {
+    ami = "ami-0c2f3d2ee24929520"
+    key_name = "ohio"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = ["sg-0bf1e4b2a2e7674af"]
+    tags = {
+        Name = "Instance_1"
+    }
+}
