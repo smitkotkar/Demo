21afae7b98753315e08becf4e1b8cb255f9895f2
smit <smitkotkar42@gmail.com>
2024-01-23 14:15:16 +0530

    jenkins installation

---
+++ Jenkins_Installation/jenkins.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region = "us-east-2"  # Replace with your desired AWS region
+}
+
+resource "aws_instance" "jenkins_instance" {
+  ami           = "ami-0c2f3d2ee24929520"  # Replace with your desired Jenkins-ready AMI ID (e.g., an Ubuntu AMI with Jenkins pre-installed)
+  instance_type = "t2.micro"                # Replace with your desired instance type
+
+  key_name      = "ohio"          # Replace with your key pair name
+  security_group_names = ["default"]  # Replace with your security group name(s)
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              sudo amazon-linux-extras install java-openjdk11 -y
+              sudo amazon-linux-extras install epel -y
+              sudo yum install -y wget
+              sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins.io/redhat/jenkins.repo
+              sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
+              sudo yum install -y jenkins
+              sudo systemctl start jenkins
+              EOF
+
+  tags = {
+    Name = "jenkins-instance"
+  }
+}
+
+output "jenkins_url" {
+  value = "http://${aws_instance.jenkins_instance.public_ip}:8080"
+}
