171701c724d958324b3fc7617b58a09b58a37129
smit <smitkotkar42@gmail.com>
2024-01-23 10:17:35 +0530

    comment edit

--- instance.tf
+++ instance.tf
@@ -2,6 +2,19 @@ provider "aws" {
     region = "us-east-2"
 }
 
+# Run command on Terminal
+# aws s3 cp terraform.tfstate s3://your-unique-s3-bucket-name/my-project/terraform.tfstate
+
+terraform {
+  backend "s3" {
+    bucket         = "terraform.tfstatefile"
+    key            = "terraform.tfstate"
+    region         = "us-east-2"  # Replace with your desired region
+    encrypt        = true
+    dynamodb_table = "terraform_locks"  # Optional: Specify a DynamoDB table for state locking
+  }
+}
+
 resource "aws_instance" "my-instance" {
     count = 2
     ami = "ami-0c2f3d2ee24929520"
